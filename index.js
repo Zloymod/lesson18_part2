e040afdfde90ad3e21f7848ca6d58dcf84032338
Denis Korolev <pnzlo@mail.ru>
2024-12-28 14:14:58 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import "./index.css";
+import JS_IMAGE from "./assets/jsLogo.webp";
+
+const jsImageHTML = document.createElement("img");
+jsImageHTML.src = JS_IMAGE;
+document.body.appendChild(jsImageHTML);
+
+console.log("Hello World!");
